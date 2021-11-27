0ce0c124771830f927302b5e2141678cc5ab31ba
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2021-11-27 13:20:01 +0530

    Create luckyboundaries.cpp

---
+++ Codechef/luckyboundaries.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int mod=1e9+7;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long int ll;
+
+int main(){
+    fast;
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<n<<"\n";
+}
+}
